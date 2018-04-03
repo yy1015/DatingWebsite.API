a29dd0e0a3e2f538f576afcb968f186aa9428eea
YANG XU <xuyangxinyi@hotmail.com>
2018-04-03 14:12:38 +1000

    update UserDto

--- Dtos/UserForDetailDto.cs
+++ Dtos/UserForDetailDto.cs
@@ -1,22 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailDto
     {
-        public int Id { get; set; }
+         public int Id { get; set; }
 
         public string Username { get; set; }
 
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -34,16 +30,8 @@ namespace DatingApp.API.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
 
-
-        public User()
-        {
-            Photos = new Collection<Photo>();
-        }
-
-        
+        public ICollection<PhotoForDetailDto> Photos { get; set; }
     }
-
-   
 }
